65690b17382d853e4b1cf345f35bc858beeca67e
Дмитрий Иванов <jackree@yandex.ru>
2019-08-16 13:20:57 +0300

    Поправлен gulp таск

--- gulpfile.js
+++ gulpfile.js
@@ -26,12 +26,10 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
     .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
